b9157f798a25ae83d67a99e9d44b78db577a3eaf
dionb112 <dion-b@hotmail.com>
2020-01-07 11:05:53 +0100

    Imported Expo project and first attempt at Video player

--- App.js
+++ App.js
@@ -1,27 +1,60 @@
-import React, { useEffect, useState, useRef } from 'react';
-import { Text, View, StyleSheet } from 'react-native';
-import Constants from 'expo-constants';
- 
- 
-const App = () => {
- return (
-   <View style={styles.container}>
-     <Text>
-       We Will Start Here
-     </Text>
-   </View>
- );
+import React from 'react';
+import { StyleSheet, View, Text, Dimensions } from 'react-native';
+import { Video } from 'expo';
+import { MaterialIcons, Octicons } from '@expo/vector-icons';
+export default class App extends React.Component {
+   render() {
+      const { width } = Dimensions.get('window');
+      return (
+         <View style={styles.container}>
+            <View>
+               <Text style={{ textAlign: 'center' }}>
+                   React Native Video</Text>
+               <Video
+                 source={{ uri:    
+        'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4' }}
+                 shouldPlay={this.state.shouldPlay}
+                 resizeMode="cover"
+                 style={{ width, height: 300 }}
+                 isMuted={this.state.mute}
+               />
+               <View style={styles.controlBar}>
+                   <MaterialIcons
+                      name={this.state.mute ? "volume-mute" :
+                          "volume-up"}
+                      size={45}
+                      color="white"
+                      onPress={this.handleVolume}
+                   />
+                   <MaterialIcons
+                      name={this.state.shouldPlay ? "pause" : 
+                           "play-arrow"}
+                      size={45}
+                      color="white"
+                      onPress={this.handlePlayAndPause}
+                   />
+               </View>
+            </View>
+        </View>
+    );
+  }
 }
- 
-export default App;
- 
 const styles = StyleSheet.create({
- container: {
-   flex: 1,
-   justifyContent: 'center',
-   alignItems: 'center',
-   paddingTop: Constants.statusBarHeight,
-   backgroundColor: '#ecf0f1',
-   padding: 8,
- },
+   container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+   },
+   controlBar: {
+      position: 'absolute',
+      bottom: 0,
+      left: 0,
+      right: 0,
+      height: 45,
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor: "rgba(0, 0, 0, 0.5)",
+   }
 });
--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,12 @@
-module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+module.exports = function(api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+  };
 };
+
+// babel config before using Expo: 
+//
+// module.exports = {
+//   presets: ['module:metro-react-native-babel-preset'],
+// };
