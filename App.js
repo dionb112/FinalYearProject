3101aa0eed60a5089bd141f151a2727514b37e64
dionb112 <dion-b@hotmail.com>
2020-01-09 13:49:47 +0100

    Scaling fixed on video

--- App.js
+++ App.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-//import YoutubePlayer from './YoutubePlayer';
+import { Dimensions, StyleSheet, Text, View } from 'react-native';
 import ExpoVideoPlayer  from './ExpoVideoPlayer';
 import { Video } from 'expo-av';
-
 
 
 export default class App extends React.Component {
@@ -12,14 +10,14 @@ export default class App extends React.Component {
       <View style={styles.container}>
         <Text>CeliApp</Text>
         <ExpoVideoPlayer/>
+        
         <Video
         source = {require('./assets/what_happens_when_you_get_glutened.mp4')}
         shouldPlay
         useNativeControls
         style={{ width: "100%", height: "50%" }}
+        resizeMode={'contain'}
       />
-
-      {/* <YoutubePlayer/> */}
       </View>
     );
   }
