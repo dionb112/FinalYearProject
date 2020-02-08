a178a77c2d62ef1f49c998df09d03d42667fe777
Dion Buckley <dion-b@hotmail.com>
2020-02-08 15:16:40 +0000

    Setting Web View to one side

--- components/QuizWebView.jsx
+++ components/QuizWebView.jsx
@@ -4,9 +4,8 @@ import { StyleSheet, Image, Text, View, Dimensions } from 'react-native';
 import { AssetUtils } from 'expo-asset-utils';
 
 
-
+/// Attempt to load the uri asynrounsly in hopes of serving the correct bundle : (
 export default class MyWeb extends Component {
-
   async getQuiz(webViewSource) {
     console.log(webViewSource);
     try {
@@ -17,17 +16,15 @@ export default class MyWeb extends Component {
     catch (e) {
       console.log('caught error', e);
     }
-
   }
 
   render() {
-    const { width } = Dimensions.get('window');
+    /// failed attemps to load the quiz and build through web view
     //const webViewSource = { html: require('../assets/index.js') () };
     //const source = require('../assets/public/index.html');
-    // console.log(webViewSource);
-
     //const webViewSource = this.getQuiz(source);
 
+    const { width } = Dimensions.get('window');
     return (
       <View style={styles.container}>
         <Text>{'Hi'}</Text>
@@ -35,16 +32,19 @@ export default class MyWeb extends Component {
           allowFileAccess={true}
           style={{ width, height: "100%" }}
           originWhitelist={['*']}
-          //Loading html file from project folder
-          source={ require('../assets/public/test.html') }
 
-          // This goes through all the way and asks for missing packages
+          //Loading html file from project folder
+          //  source={ require('../assets/public/test.html') }
+
+          source= {{uri: 'http://34.246.173.61:3000/'}} // if the server is started the quiz can be loaded here responsively!
+
+          // This one goes through all the way and asks for missing packages
           // After installing all dependencies loops back around to this require
           // Saying that something is wrong in the dependecy bundle
-          //source={{ html: require('../assets/index.js') () }}
+          //    source={{ html: require('../assets/index.js') () }}
 
-
-          //source={{ html: require('../assets/public/boot.js')() }}
+          // attempt to serve the app through a boot.js
+          // source={{ html: require('../assets/public/boot.js')() }}
 
 
           useWebKit
