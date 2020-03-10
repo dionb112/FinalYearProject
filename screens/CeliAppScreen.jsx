18c578ee2fffd5600e6f1604342dc3b3c0d247c4
dionb112 <dion-b@hotmail.com>
2020-03-10 23:04:34 +0000

    first use of custom font using expo-font, currently same as Vogue

--- screens/CeliAppScreen.jsx
+++ screens/CeliAppScreen.jsx
@@ -1,43 +1,63 @@
 import React from 'react';
-import {
-  Button,
-  StatusBar,
-  StyleSheet,
-  View,
-  Text,
-} from 'react-native';
+import { Button, StatusBar, StyleSheet, View, Text, } from 'react-native';
+import * as Font from 'expo-font'
 import Coin from '../components/Coin'
 
 export default class CeliAppScreen extends React.Component {
-    static navigationOptions = {
-      title: 'CeliApp    ',
-    };
-  
-    render() {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      fontLoaded: false
+    }
+  }
+
+
+  static navigationOptions = {
+    title: 'CeliApp    ',
+  };
+
+  render() {
+    if (this.state.fontLoaded) {
       return (
         <View style={styles.container}>
           <Text style={styles.title}>{'CeliApp Home Screen'}</Text>
-          <Coin/>
+          <Coin />
           <Button title="Knowledge Center  " onPress={this._knowledge} />
           <StatusBar barStyle="default" />
         </View>
       );
     }
-  
-    _knowledge = () => {
-      this.props.navigation.navigate('Home');
-    };
+    else {
+      return (<View />);
+    }
   }
 
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    title: {
-      fontSize: 30,
-      textAlign: 'center',
-      margin: 20,
-    }
-  });
+  _knowledge = () => {
+    this.props.navigation.navigate('Home');
+  };
+
+  // something like this for custom fonts..
+  async componentDidMount() {
+    await Font.loadAsync({
+      PlayfairDisplay : require('../assets/fonts/PlayfairDisplay-Regular.otf')
+    })
+    this.setState({ fontLoaded: true })
+  }
+}
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 30,
+    textAlign: 'center',
+    margin: 20,
+    fontFamily: 'PlayfairDisplay'
+  }
+});
